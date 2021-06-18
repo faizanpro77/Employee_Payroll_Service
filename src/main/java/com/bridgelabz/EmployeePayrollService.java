df31d63ddb681903759c2dd87264bc2c02fb4689
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-18 21:50:07 +0530

    Welcome to employee payroll program

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee payroll service");
+    }
+}
